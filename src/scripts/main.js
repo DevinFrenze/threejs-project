173b20a8c206959b8b5bb7536392f7bbfc86cf6a
Devin Frenze <devinfrenze@Devins-MBP.att.net>
2016-11-30 14:17:14 -0800

    cleanup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,21 @@
 import AbstractApplication from 'scripts/views/AbstractApplication'
-const glslify = require('glslify')
-const shaderVert = glslify('./../shaders/custom.vert')
-const shaderFrag = glslify('./../shaders/custom.frag')
 
 class Main extends AbstractApplication {
-    constructor(){
+  constructor(){
 
-        super();
+    super();
 
-        var texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
+    var texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
 
-        var geometry = new THREE.BoxGeometry( 200, 200, 200 );
-        var material = new THREE.MeshBasicMaterial( { map: texture } );
+    var geometry = new THREE.BoxGeometry( 200, 200, 200 );
+    var material = new THREE.MeshBasicMaterial( { map: texture } );
 
-        var material2 = new THREE.ShaderMaterial({
-            vertexShader: shaderVert,
-            fragmentShader: shaderFrag
-        });
+    this._mesh = new THREE.Mesh( geometry, material );
+    this._scene.add( this._mesh );
 
+    this.animate();
 
-        this._mesh = new THREE.Mesh( geometry, material2 );
-        this._scene.add( this._mesh );
-
-        this.animate();
-
-    }
+  }
 
 }
 export default Main;
