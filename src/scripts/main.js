b2a31b65bca43ad82722985bcb4efc06a30a40f9
Devin Frenze <devinfrenze@Devins-MacBook-Pro.local>
2017-07-21 14:59:44 -0700

    fix remote color control AND add make material color bindings such that we don't need to update them all the time

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,7 @@ import 'postprocessing/GlitchPass';
 
 import AudioAnalyser from 'scripts/plugins/audioAnalyser';
 import Controls from 'scripts/controls/mouse_from_center';
+import Text from 'scripts/objects/text';
 
 const {
   IcosahedronGeometry,
@@ -24,31 +25,45 @@ class Main extends AbstractApplication {
 
   initGui() {
     const gui = new dat.GUI();
-    this._uiColor = new UIColor(gui);
+    this.uiColor1 = new UIColor(gui, "color1");
+    this.uiColor2 = new UIColor(gui, "color2", 0, 0, 0);
+    this.uiColor3 = new UIColor(gui, "color3");
   }
 
   initScene() {
-    const controls = new Controls(this, this.scene, 1);
+    // const controls = new Controls(this, this.scene, 1);
 
-    this.camera.position.z = 250;
-    this.camera.position.y = 100;
+    const material1 = new MeshBasicMaterial({
+      transparent: true,  // transparent true enables blending
+    });
+    material1.color = this.uiColor1.color; // binds color to ui
 
-    this._material = new MeshBasicMaterial({ wireframe: false });
     const icoGeometry = new IcosahedronGeometry(200, 2);
-    this.addToScene(new Mesh(icoGeometry, this._material));
+    this.addToScene(new Mesh(icoGeometry, material1));
 
     const wireMaterial = new MeshBasicMaterial({
+      color: this.uiColor2.color,
       wireframe: true,
-      color: "#000000"
+      transparent: true
     });
-
+    wireMaterial.color = this.uiColor2.color;
     this.addToScene(new Mesh(icoGeometry, wireMaterial));
+
+    const material3 = new MeshBasicMaterial({
+      transparent: true,
+      blending: THREE.CustomBlending,
+      blendEquation: THREE.SubtractEquation,
+      blendSrc: THREE.DstAlphaFactor,
+      blendDst: THREE.SrcAlphaFactor
+    });
+    material3.color = this.uiColor3.color;
+
+
+    const textMesh = new Text().getMesh(material3);
+    textMesh.position.z = 200;
+    this.addToScene(textMesh);
   }
 
-  update() {
-    super.update();
-    this._material.color = this._uiColor.toFloat();
-  }
 }
 
 export default Main;
--- src/scripts/ui/ui_color.js
+++ src/scripts/ui/ui_color.js
@@ -1,31 +1,50 @@
 class UIColor {
-  constructor(gui, name = 'rgbColor', r = 255, g = 255, b = 255, remote = false) {
+  constructor(gui, name = 'rgbColor', r = 255, g = 255, b = 255, remote = true) {
     this._name = name;
     this[name] = [ r, g, b ];
     this._colorControl = gui.addColor(this, this._name); 
-    if (remote) removeControl();
+    this._color = new THREE.Color();
+
+    if (remote) {
+      this.remoteControl();
+    }
+
+    this._colorControl.onChange((value) => {
+      if (remote) localStorage.setItem(this._name, value);
+      this.updateColorObject();
+    });
+    this.updateColorObject();
   }
 
-  get color() {
+  updateColorObject() {
+    this._color.fromArray(this.toFloat());
+  }
+
+  get colorArray() {
     return this[this._name];
   }
 
+  get color() {
+    return this._color;
+  }
+
   toFloat() {
-    return this.color.map((v) => v / 255);
+    return this.colorArray.map((v) => v / 255);
   }
 
   remoteControl() {
-    localStorage.setItem(this._name, this.color);
-
-    this._colorControl.onChange((value) => {
-      localStorage.setItem(this._name, value);
-    });
-
-    onStorageChange = () => {
+    const onStorageChange = () => {
       let color = localStorage.getItem(this._name);
-      color = color.split(',').map((v) => parseInt(v));
-      this._colorControl.setValue(color);
+      if (color) {
+        color = color.split(',').map((v) => parseInt(v));
+        this._colorControl.setValue(color);
+        this.updateColorObject();
+      } else {
+        localStorage.setItem(this._name, this.colorArray);
+      }
     };
+
+    onStorageChange();
 
     window.addEventListener( 'storage', onStorageChange, false);
   }
