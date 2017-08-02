e9bb73ed97432ee7fb57a2d877935220261dd786
Devin Frenze <devinfrenze@Devins-MBP.att.net>
2017-08-02 00:05:38 -0700

    streamline and add glitch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,15 +9,12 @@ import Terrain from 'scripts/objects/Terrain';
 import GridTexture from 'scripts/objects/GridTexture';
 
 import 'shaders/DigitalGlitch';
-import 'shaders/DotScreenShader';
-import 'shaders/ColorifyShader';
 import 'shaders/FXAAShader';
 import 'shaders/FilmShader';
 import 'shaders/RGBShiftShader';
 
 import 'postprocessing/FilmPass';
 import 'postprocessing/GlitchPass';
-import 'postprocessing/OutlinePass';
 
 import 'loaders/AssimpJSONLoader';
 
@@ -107,10 +104,10 @@ class Main extends AbstractApplication {
   }
 
   postProcessing() {
-    // also include glitch
     // TODO figure out how to make dynamic
+    const glitchPass = new THREE.GlitchPass();
+    this.addToRenderChain( glitchPass );
 
-    /*
     // adds horizontal lines to screen, looks rly good with rgbShift
     const film = new THREE.FilmPass(0.35, 0.025, 648, false );
     this.addToRenderChain( film );
@@ -119,43 +116,6 @@ class Main extends AbstractApplication {
     const rgbShift = new THREE.ShaderPass( THREE.RGBShiftShader );
     rgbShift.uniforms[ 'amount' ].value = 0.0015;
     this.addToRenderChain( rgbShift );
-    */
-
-    /////////////////////////
-    //
-    // dots casts everything to black and white, not exactly what we want rn
-    /*
-    const dots = new THREE.ShaderPass( THREE.DotScreenShader );
-    dots.uniforms[ 'scale' ].value = 4;
-    this.addToRenderChain( dots );
-    */
-
-    // colorify casts everything to one color, not useful rn
-    /*
-    const colorify = new THREE.ShaderPass( THREE.ColorifyShader );
-    colorify.uniforms[ 'color' ] = new THREE.Uniform( new THREE.Color( 1, 0.8, 0.8) );
-    this.addToRenderChain( colorify );
-    */
-
-    const outlinePass = new THREE.OutlinePass( new THREE.Vector2(window.innerWidth, window.innHeight), this.scene, this.camera);
-    this.addToRenderChain( outlinePass );
-
-    var loader = new THREE.TextureLoader();
-
-    // load a resource
-    loader.load(
-      'textures/tri_pattern.jpg', // resource URL
-      ( texture ) => {            // Function when resource is loaded
-	outlinePass.patternTexture = texture;
-	texture.wrapS = THREE.RepeatWrapping;
-	texture.wrapT = THREE.RepeatWrapping;
-      }
-    );
-
-    const fxaa = new THREE.ShaderPass(THREE.FXAAShader);
-    fxaa.uniforms['resolution'].value.set(1 / window.innerWidth, 1 / window.innherHeight );
-    this.addToRenderChain( fxaa );
-    // also should add on window listeners
   }
 }
 
